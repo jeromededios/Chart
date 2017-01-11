3bfa341de9a51fb5e071a86a5e8a56610554aa5c
jeromededios <jeromededios@protonmail.com>
2017-01-11 18:39:44 +0800

    Initial commit
    Engineering Technical Test 2 (Cart)

---
+++ src/main/java/com/dedios/chart/App.java
@@ -0,0 +1,13 @@
+package com.dedios.chart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
